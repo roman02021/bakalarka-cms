039172dadc47845257aeecf9b2889311b05f3ff5
Roman Zemanik <romanzemanik@gmail.com>
2024-04-11 20:47:50 +0200

    changed uppercase import to lowercase

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from './entities/User.entity';
+import { User } from './entities/user.entity';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
